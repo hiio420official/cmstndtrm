b1407e753cb4cb0f2c2d6da26a8f0216dfbcfae6
hiio420official <hiio420.official@gmail.com>
2024-03-19 01:15:10 +0900

    TEST

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ main.py
 import math
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from libs import ExcelUtils
 
 # 엑셀 파일 불러오기
 
-file_path = "F:\\[01]project\\commonStandardTerm\\upload\\(붙임)공공데이터 공통표준용어(2022.7월).xlsx"
+file_path = "C:\\Users\\chohunchang\\Desktop\\project\\cmStrdTrm\\upload\\(붙임)공공데이터 공통표준용어(2022.7월).xlsx"
 excel_utils = ExcelUtils()
 df_dict = excel_utils.read_excel(file_path)
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    data_id: int = 0
+    page: int = 1
+    limit: int = 10
 
 
 @app.get("/")
@@ -35,18 +42,17 @@ def read_root(data_id: int = 0, page: int = 1, limit: int = 10):
     total_size = df.shape[0]
     first_page = 1
     last_page = math.ceil(total_size / limit)
+    df = df[df.iloc[:,2].str.contains("검색")]
     df_sliced = df[first_idx:last_idx + 1].copy()
 
-    df_sliced["번호"] = [i for i in range(total_size - (page - 1) * limit, total_size - (page - 1) * limit - df_sliced.shape[0], -1)]
+    df_sliced["번호"] = [i for i in
+                       range(total_size - (page - 1) * limit, total_size - (page - 1) * limit - df_sliced.shape[0], -1)]
     data = []
 
     if df_sliced.shape[0] != 0:
         data = df_sliced.to_dict("records")
 
-
     return {"sheetName": excel_utils.sheet_names[data_id], "data_id": data_id, "page": page, "limit": limit,
             "first_page": first_page, "last_page": last_page, "firstIdx": first_idx, "lastIdx": last_idx,
             "totalSize": total_size,
             "data": data}
-
-
